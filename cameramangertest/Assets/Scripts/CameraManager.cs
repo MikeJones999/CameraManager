eeb57f6f8350fab6a2b6b440a1cfd266ae2c9886
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2017-04-05 23:04:35 +0100

    Initial commit - basic setup created 2 weapons that when clicked on the camera will focus on, 3rd object added(capsule) to bring camera back to starting position

---
+++ cameramangertest/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    public static CameraManager instance;
+    public GameObject cameraDefault;
+
+
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void DefaultPosition()
+    {
+        Camera.main.transform.position = cameraDefault.transform.position;
+        Camera.main.transform.rotation = cameraDefault.transform.rotation;
+    }
+
+    public void FocusMe(GameObject weapon)
+    {
+        var pos = weapon.transform.position;
+
+        var offset = new Vector3(pos.x -8, pos.y +1, pos.z -5);
+
+        Camera.main.transform.position = offset;
+        Camera.main.transform.LookAt(pos);
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+
+        Camera.main.transform.position = cameraDefault.transform.position;
+       // Camera.main.transform.rotation = cameraDefault.transform.rotation;
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ cameramangertest/Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+
+	private GameObject weapon;
+
+	// Use this for initialization
+	void Start () {
+		weapon = transform.gameObject;
+		Debug.Log("weapon - " + weapon);
+			
+			//GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnMouseDown()
+	{
+		CameraManager.instance.FocusMe(weapon);
+	}
+
+}
